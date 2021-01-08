d26766083eb1ba6dfe9b1954683c4770aaf0df4b
Ruben Verborgh <ruben@verborgh.org>
2021-01-08 23:28:42 +0100

    fix: Guard converters against invalid input.

--- src/HueToQuadConverter.ts
+++ src/HueToQuadConverter.ts
@@ -1,18 +1,13 @@
-import { PassThrough } from 'stream';
 import { namedNode, literal } from '@rdfjs/data-model';
 import {
-  CONTENT_TYPE, INTERNAL_QUADS, PREFERRED_PREFIX_TERM, RDF,
+  INTERNAL_QUADS, PREFERRED_PREFIX_TERM, RDF,
+  BasicRepresentation,
   RepresentationMetadata,
   TypedRepresentationConverter,
-  guardStream,
   pushQuad,
-  readableToString,
+  transformSafely,
 } from '@solid/community-server';
-import type {
-  Representation,
-  RepresentationConverterArgs,
-  ResourceIdentifier,
-} from '@solid/community-server';
+import type { Representation, RepresentationConverterArgs } from '@solid/community-server';
 import { DOGONT, HUE, RDFS, stateMapping } from './Vocabularies';
 import { HueContentType } from './Constants';
 
@@ -20,45 +15,36 @@ import { HueContentType } from './Constants';
  * Converts the Philips Hue JSON format to quads.
  */
 export class HueToQuadConverter extends TypedRepresentationConverter {
-  public async getInputTypes(): Promise<Record<string, number>> {
-    return { [HueContentType]: 1 };
-  }
-
-  public async getOutputTypes(): Promise<Record<string, number>> {
-    return { [INTERNAL_QUADS]: 0.5 };
+  public constructor() {
+    super(HueContentType, INTERNAL_QUADS);
   }
 
   public async handle({ identifier, representation }: RepresentationConverterArgs): Promise<Representation> {
-    // Create the resulting representation and its metadata
-    const data = guardStream(new PassThrough({ objectMode: true }));
-    const metadata = new RepresentationMetadata(representation.metadata, { [CONTENT_TYPE]: INTERNAL_QUADS });
+    const metadata = new RepresentationMetadata(representation.metadata, INTERNAL_QUADS);
     metadata.addQuad(DOGONT.terms.namespace, PREFERRED_PREFIX_TERM, 'dogont');
     metadata.addQuad(RDFS.terms.namespace, PREFERRED_PREFIX_TERM, 'rdfs');
     metadata.addQuad(HUE.terms.namespace, PREFERRED_PREFIX_TERM, 'ph');
 
-    // Parse the source asynchronously
-    setImmediate(async(): Promise<void> => {
-      const lights = JSON.parse(await readableToString(representation.data));
-      this.addQuadsFromLights(data, identifier, lights);
+    // Transform the JSON into RDF quads
+    const json: string[] = [];
+    const data = transformSafely(representation.data, {
+      readableObjectMode: true,
+      transform: (buffer): any => json.push(buffer),
+      flush(): void {
+        // Generate RDF quads for every light
+        const lights: Record<string, any> = JSON.parse(json.join(''));
+        for (const [key, light] of Object.entries(lights)) {
+          // Generate the type and label
+          const id = namedNode(`${identifier.path}#${encodeURIComponent(key)}`);
+          pushQuad(this, id, RDF.terms.type, DOGONT.terms.Lamp);
+          pushQuad(this, id, RDFS.terms.label, literal(light.name));
+          // Generate state properties
+          for (const [predicate, { field, dataType }] of Object.entries(stateMapping)) {
+            pushQuad(this, id, predicate, literal(`${light.state[field]}`, dataType));
+          }
+        }
+      },
     });
-
-    return { binary: false, data, metadata };
-  }
-
-  protected addQuadsFromLights(data: PassThrough, source: ResourceIdentifier, lights: Record<string, any>): void {
-    for (const id of Object.keys(lights)) {
-      this.addQuadsFromLight(data, source, { id, ...lights[id] });
-    }
-    data.push(null);
-  }
-
-  protected addQuadsFromLight(data: PassThrough, source: ResourceIdentifier, light: Record<string, any>): void {
-    const id = namedNode(`${source.path}#${encodeURIComponent(light.id)}`);
-    pushQuad(data, id, RDF.terms.type, DOGONT.terms.Lamp);
-    pushQuad(data, id, RDFS.terms.label, literal(light.name));
-    for (const predicate of Object.keys(stateMapping)) {
-      const { field, dataType } = stateMapping[predicate];
-      pushQuad(data, id, predicate, literal(`${light.state[field]}`, dataType));
-    }
+    return new BasicRepresentation(data, metadata);
   }
 }
